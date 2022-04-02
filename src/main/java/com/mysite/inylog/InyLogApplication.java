6de5984cf34f1d95e3d6b04cbae367387481a008
unknown <pmsu2007@naver.com>
2022-04-02 22:42:35 +0900

    initail commit

---
+++ src/main/java/com/mysite/inylog/InyLogApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.inylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InyLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite/inylog/InyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.inylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
