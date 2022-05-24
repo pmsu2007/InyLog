5e5b6b0abec8a89792e9c6b03e79173296d436b0
KYJ <thjbjs@outlook.com>
2022-05-24 18:35:09 +0900

    branch test

---
+++ src/main/java/com/mysite/inylog/form/MemberCreatForm.java
@@ -0,0 +1,23 @@
+package com.mysite.inylog.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MemberCreatForm {
+    //
+//    @NotEmpty(message = "닉네임은 필수항목입니다.")
+//    private String userId;
+//
+//    @NotEmpty(message = "비밀 번호는 필수항목입니다.")
+//    private String password1;
+//
+//    @NotEmpty(message = "비밀 번호를 다시 한번 입력하세요.")
+//    private String password2;
+//
+//    @NotEmpty(message = "이메일은 필수항목입니다.")
+//    private String email;
+
+
+}
--- src/main/java/com/mysite/inylog/service/MemberService.java
+++ src/main/java/com/mysite/inylog/service/MemberService.java
@@ -1,10 +1,34 @@
 package com.mysite.inylog.service;
 
+import com.mysite.inylog.domain.Member;
+import com.mysite.inylog.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원가입
+    public Member create(String newUserId, String newEmail, String newPassWord){
+        Member member = new Member();
+        member.setEmail(newEmail);
+        member.setUserId(newUserId);
+        //보안 문제 논의
+        //member.setPassword(newPassWord);
+        this.memberRepository.save(member);
+        return member;
+    }
+
+
+    //로그인
+
+
+    //이메일으로 아이디 찾기
+
+    //비밀번호
+
 
 }
