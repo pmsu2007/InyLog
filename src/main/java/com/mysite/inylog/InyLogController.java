ef2e2f7fcba997f4e562834269e92bda005a3bbd
unknown <pmsu2007@naver.com>
2022-05-29 03:37:10 +0900

    feat : main ãblog screen

--- src/main/java/com/mysite/inylog/InyLogController.java
+++ src/main/java/com/mysite/inylog/InyLogController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class InyLogController {
-    @RequestMapping("/home")
-    @ResponseBody
-    public String hello() {
-        return "Hello InyLog";
+    @RequestMapping("/")
+    public String blog() {
+        return "blog_main";
     }
 }
