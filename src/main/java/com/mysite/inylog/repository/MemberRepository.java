a192e495a0f6eba64c137d683ce44b206a76e931
KYJ <thjbjs@outlook.com>
2022-05-28 18:09:33 +0900

    coding Member login

--- src/main/java/com/mysite/inylog/repository/MemberRepository.java
+++ src/main/java/com/mysite/inylog/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.mysite.inylog.repository;
 import com.mysite.inylog.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByusername(String userId);
 }
